f720965b11f3af0ba7c3dc1e3e3b5c7c3db296d1
mAlishera <rooh@mail.ru>
2017-02-18 21:15:05 +0300

    second post

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,6 +13,11 @@ ShemereyPosts.Repo.insert!(%ShemereyPosts.Post{title: "Composite primary keys in
 	                                           subtitle: "Ecto and ActiveRecord behaive differently",
 	                                           path: "composite_primaty_keys_in_rails_and_phoenix_apps",
 	                                           tags: ["elixir", "ruby"]})
+ShemereyPosts.Repo.insert!(%ShemereyPosts.Post{title: "Custom middleware with rescue database connection error and redirection to config form.",
+                                             subtitle: "Establish db connection in runtime when database.yml config invalid.",
+                                             path: "custom_middleware_with_rescue_db_connection_error_and_redirection",
+                                             tags: ["ruby"]})
+
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
